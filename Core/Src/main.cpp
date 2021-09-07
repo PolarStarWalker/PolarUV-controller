83e745a02d3c40fedf59509fad6a2dbc79c065ac
motov.s <motov.s@edu.narfu.ru>
2021-09-07 15:27:25 +0300

    перевод команды шим в DShot

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -90,8 +90,23 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     }
 }
 
-uint16_t CreatePacket(uint16_t gas, bool isNeedTelemetry){
+uint16_t CreatePacket(uint16_t gas, bool isNeedTelemetry) {
+    uint16_t packet = gas + 47;
 
+    packet <<= 1;
+
+    if (isNeedTelemetry)
+        ++packet;
+
+    int csum = 0;
+    int csum_data = packet;
+    for (int i = 0; i < 3; i++) {
+        csum ^= csum_data; // xor data by nibbles
+        csum_data >>= 4;
+    }
+    csum &= 0xf;
+    // append checksum
+    return (packet << 4) | csum;
 }
 
 
@@ -101,45 +116,44 @@ uint16_t CreatePacket(uint16_t gas, bool isNeedTelemetry){
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+int main(void) {
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_SPI1_Init();
-  MX_SPI2_Init();
-  MX_TIM1_Init();
-  MX_TIM2_Init();
-  MX_TIM3_Init();
-  MX_TIM4_Init();
-  MX_UART4_Init();
-  MX_UART7_Init();
-  MX_UART8_Init();
-  MX_USART2_UART_Init();
-  MX_USART3_UART_Init();
-  MX_TIM5_Init();
-  MX_USB_OTG_FS_PCD_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_SPI1_Init();
+    MX_SPI2_Init();
+    MX_TIM1_Init();
+    MX_TIM2_Init();
+    MX_TIM3_Init();
+    MX_TIM4_Init();
+    MX_UART4_Init();
+    MX_UART7_Init();
+    MX_UART8_Init();
+    MX_USART2_UART_Init();
+    MX_USART3_UART_Init();
+    MX_TIM5_Init();
+    MX_USB_OTG_FS_PCD_Init();
+    /* USER CODE BEGIN 2 */
 
     HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
     HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
@@ -158,12 +172,12 @@ int main(void)
     HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
 
     HAL_TIM_Base_Start_IT(&htim5);
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
 
-    MotorsStruct MotorsStructData{};
+    MotorsStruct motorsStructData{};
     uint8_t msg[MotorsStructLenMessage * 2]{};
 
     HAL_SPI_Receive_IT(&hspi1, msg, MotorsStructLenMessage * 2);
@@ -180,40 +194,27 @@ int main(void)
 
             for (size_t i = 0; i < MotorsStructLenMessage * 2 - 1; i++) {
                 if (msg[i] == 's' && msg[i + 1] == 's') {
-                    memcpy(&MotorsStructData, &msg[i + 2], MotorsStructLen);
+                    memcpy(&motorsStructData, &msg[i + 2], MotorsStructLen);
                     i = MotorsStructLenMessage * 2;
                 }
             }
 
-            uint16_t packet = MotorsStructData.PacketArray[0] + 47;
 
-            packet <<= 1;
+            TIM1->CCR1 = motorsStructData.PacketArray[0] * 6 + 9000;
+            TIM1->CCR2 = motorsStructData.PacketArray[1] * 6 + 9000;
+            TIM1->CCR3 = motorsStructData.PacketArray[2] * 6 + 9000;
 
-            int csum = 0;
-            int csum_data = packet;
-            for (int i = 0; i < 3; i++) {
-                csum ^= csum_data; // xor data by nibbles
-                csum_data >>= 4;
-            }
-            csum &= 0xf;
-            // append checksum
-            packet = (packet << 4) | csum;
+            TIM2->CCR1 = motorsStructData.PacketArray[3] * 6 + 9000;
+            TIM2->CCR2 = motorsStructData.PacketArray[4] * 6 + 9000;
+            TIM2->CCR3 = motorsStructData.PacketArray[5] * 6 + 9000;
 
-            TIM1->CCR1 = MotorsStructData.PacketArray[0] * 6 + 9000;
-            TIM1->CCR2 = MotorsStructData.PacketArray[1] * 6 + 9000;
-            TIM1->CCR3 = MotorsStructData.PacketArray[2] * 6 + 9000;
+            TIM3->CCR1 = motorsStructData.PacketArray[6] * 6 + 9000;
+            TIM3->CCR2 = motorsStructData.PacketArray[7] * 6 + 9000;
+            TIM3->CCR3 = motorsStructData.PacketArray[8] * 6 + 9000;
 
-            TIM2->CCR1 = MotorsStructData.PacketArray[3] * 6 + 9000;
-            TIM2->CCR2 = MotorsStructData.PacketArray[4] * 6 + 9000;
-            TIM2->CCR3 = MotorsStructData.PacketArray[5] * 6 + 9000;
-
-            TIM3->CCR1 = MotorsStructData.PacketArray[6] * 6 + 9000;
-            TIM3->CCR2 = MotorsStructData.PacketArray[7] * 6 + 9000;
-            TIM3->CCR3 = MotorsStructData.PacketArray[8] * 6 + 9000;
-
-            TIM4->CCR1 = MotorsStructData.PacketArray[9] * 0.6 + 9000;
-            TIM4->CCR2 = MotorsStructData.PacketArray[10] * 0.6 + 9000;
-            TIM4->CCR3 = MotorsStructData.PacketArray[11] * 0.6 + 9000;
+            TIM4->CCR1 = motorsStructData.PacketArray[9] * 0.6 + 9000;
+            TIM4->CCR2 = motorsStructData.PacketArray[10] * 0.6 + 9000;
+            TIM4->CCR3 = motorsStructData.PacketArray[11] * 0.6 + 9000;
 
             TicCount = 0;
         }
@@ -236,67 +237,64 @@ int main(void)
             TIM4->CCR3 = NEUTRAL;
         }
 
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Supply configuration update enable
-  */
-  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Supply configuration update enable
+    */
+    HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 2;
-  RCC_OscInitStruct.PLL.PLLN = 64;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
-  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-  RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+    while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 2;
+    RCC_OscInitStruct.PLL.PLLN = 64;
+    RCC_OscInitStruct.PLL.PLLP = 2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+    RCC_OscInitStruct.PLL.PLLFRACN = 0;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
+                                  | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+    RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -307,14 +305,13 @@ void SystemClock_Config(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
